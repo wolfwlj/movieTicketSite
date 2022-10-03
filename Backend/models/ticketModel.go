65559fd13db9273c0d3e9faa7bad4a41138d0dc7
wolfwlj <86714@glr.nl>
2022-10-03 11:41:51 +0200

    new prod

--- Backend/models/ticketModel.go
+++ Backend/models/ticketModel.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Ticket struct {
 	gorm.Model
-	Reservation_state string `gorm:"default:'available';type:text"` // "reserved" or "available"
+	Reservation_state string `gorm:"default:'available';type:varchar(255)"` // "reserved" or "available"
 	Room_id_fk        uint
 	Room              Room `gorm:"foreignKey:room_id_fk"`
 	User_id_fk        *uint
