5e5871a49e4a4cebb4b34630e783c56cf4d96097
wolfwlj <86714@glr.nl>
2022-10-03 11:39:48 +0200

    x

--- Backend/models/userModel.go
+++ Backend/models/userModel.go
@@ -6,7 +6,7 @@ type User struct {
 	gorm.Model
 	Username   string `gorm:"unique;type:varchar(255)"`
 	Password   string `json:"-"`
-	Email      string `gorm:"unique;type:text"`
+	Email      string `gorm:"unique;type:varchar(555)"`
 	First_name string `gorm:"type:text"`
 	Last_name  string `gorm:"type:text"`
 	Birthdate  string `gorm:"type:date"`
